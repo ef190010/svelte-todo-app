c6c49ee440041fde716aa5aa7c509ffa75b4406e
ito.shunsuke <ef190010@gmail.com>
2026-05-23 11:42:20 +0900

    markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+    <h1>Todo App</h1>
+    <input type="text" placeholder="新しいtodo..."/>
+    <button>追加</button>
+
+    <ul>
+        <li>
+            <input type="checkbox" />
+            <span>ミーティング資料を作る</span>
+            <button>削除</button>
+        </li>
+        <li>
+            <input type="checkbox" />
+            <span class="done">プルリクエストをレビューする</span>
+            <button>削除</button>
+        </li>
+        <li>
+            <input type="checkbox" />
+            <span>本番リリースする</span>
+            <button>削除</button>
+        </li>
+    </ul>
+</main>
+
+<style>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
